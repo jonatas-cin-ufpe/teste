dee334ae84adac379484cba1cc7259cd1c4aa5ce
Jônatas Costa <jcv@cin.ufpe.br>
2020-01-13 22:27:32 -0300

    Adicionei o Bom dia

--- Program.cs
+++ Program.cs
@@ -11,9 +11,12 @@ namespace DateT_TimeS
             DateTime c1 = new DateTime(1990, 10, 5, 13, 5, 58, DateTimeKind.Utc);
             DateTime d2 = DateTime.ParseExact("10/05/1990 19:00:23", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
+
             Console.WriteLine(d1);
             Console.WriteLine(c1);
             Console.WriteLine(d2);
+            Console.WriteLine("Bom dia");
+
         }
     }
 }
